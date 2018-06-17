aa17bf6b187467c944d6fac8682e0245cc798434
saravmani <saravmanicloud@gmail.com>
2018-06-17 10:55:23 +0530

    from home laptop server

---
+++ LAB.FullFramework.Language.Console.AllTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB.FullFramework.Language.Console.AllTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+        }
+    }
+}
